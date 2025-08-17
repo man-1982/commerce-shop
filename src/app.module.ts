20896282625fea8a855660ca6834fbd210077c83
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-17 00:18:02 -0700

    Init product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { ProductService } from './product/product.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
