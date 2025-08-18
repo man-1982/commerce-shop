5c3d5b01241e1995ee10e641ca187725af98f460
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-18 15:24:18 -0700

    Implemented Cart and CartItem dto

---
+++ src/cart/dto/cart.dto.ts
@@ -0,0 +1,75 @@
+import { PartialType } from '@nestjs/swagger';
+
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+
+export class CartDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 1,
+    description: 'The unique identifier of the cart.',
+  })
+  cid: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'fd3e4f36-b9b6-4937-959f-ead5ba6ed4b2',
+    description: 'The unique uuid of the cart.',
+  })
+  uuid: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 1,
+    description: 'The unique identifier of the user who owns the cart.',
+  })
+  uid: number;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiProperty({
+    example: true,
+    description: 'The true or false status of the cart.',
+  })
+  @Type(() => Boolean)
+  status?: boolean = true;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '2023-01-01T12:00:00Z',
+    description: 'The creation date of the cart.',
+  })
+  createdAt: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '2023-01-01T12:00:00Z',
+    description: 'The last update date of the cart.',
+  })
+  updatedAt: Date;
+}
+
+export class CreateCartDto extends OmitType(CartDto, [
+  'cid',
+  'uuid',
+  'createdAt',
+  'updatedAt',
+] as const) {}
+
+export class UpdateCartDto extends PartialType(CreateCartDto) {}
