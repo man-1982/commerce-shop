1905b8037d7da7819696ee4fdeb236624f9f019d
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-17 19:34:00 -0700

    Implemented prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
-import { ProductService } from './product/product.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProductsModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, ProductService],
+  providers: [AppService],
 })
 export class AppModule {}
