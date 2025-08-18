86b6c2911a9ec74e441ea889d99da97db09e97e7
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-17 19:39:36 -0700

    Switched on using global Pipes for all requests

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-    .setTitle('Api documentation')
+    .setTitle('API Documentation')
     .setDescription('Documentation')
     .build();
   const doc = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, doc);
+  //Use global pipes in the DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
