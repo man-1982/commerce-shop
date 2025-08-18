148ff3af953f65e329a05d9ac4edfcb41c9eaca7
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-18 00:20:14 -0700

    Added user module to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProductsModule,
     PrismaModule,
+    UserModule,
   ],
 })
 export class AppModule {}
