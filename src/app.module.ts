23fd7dc72e95be861c55f0601d8c33e9e6985e58
Andrii Kocherhin <andriikocherhin@webadmins-air.phsabc.ehcnet.ca>
2025-08-20 16:12:58 -0700

    Introduced event emitters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import * as process from 'node:process';
 
 @Module({
   imports: [
+    UserModule,
+    PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     ProductsModule,
-    PrismaModule,
-    UserModule,
     CartModule,
+    EventEmitterModule.forRoot({
+      verboseMemoryLeak: process.env.NODE_ENV === 'development',
+      ignoreErrors: process.env.NODE_ENV === 'development',
+    }),
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
