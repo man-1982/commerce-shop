7d7725bc9b45627a5ef0fa99b8b6bef353700b5e
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-22 10:35:35 -0700

    Minor change to improve code readability

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,35 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
-export * from '../../generated/prisma';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+export * from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  // constructor() {
+  //   // Dev-only query logging (helpful for debugging)
+  //   if (process.env.NODE_ENV === 'development') {
+  //     // log levels if needed: ['query', 'info', 'warn', 'error']
+  //     super({ log: ['warn', 'error'] });
+  //     // @ts-expect-error
+  //     this.$on('query', (e) => {
+  //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //       // @ts-expect-error
+  //       console.log('Query: ' + e.query);
+  //       // @ts-expect-error
+  //       console.log('Params: ' + e.params);
+  //       // @ts-expect-error
+  //       console.log('Duration: ' + e.duration + 'ms');
+  //     });
+  //   }
+  // }
+
   async onModuleInit() {
-    await this.$connect();
+    await super.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await super.$disconnect();
   }
 }
