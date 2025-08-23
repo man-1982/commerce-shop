cf4e4f95a26d3fee6e619e68b6fa28a825872267
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-22 21:59:04 -0700

    Rollback async and added comments

--- src/products/listeners/product.listener.ts
+++ src/products/listeners/product.listener.ts
@@ -6,26 +6,48 @@ import { ProductsService } from '../products.service';
 export class ProductListener {
   constructor(private readonly productsService: ProductsService) {}
 
+  /**
+   * On Cart items update
+   * @param event
+   */
   @OnEvent('cart.items.updated', { async: true })
-  handleCartItemsUpdated(event: { item: { pid: number; quantity: number } }) {
-    await this.productsService.updateQuantity(
+  async handleCartItemsUpdated(event: {
+    item: { pid: number; quantity: number };
+  }) {
+    const updatedProduct = await this.productsService.updateQuantity(
       event.item.pid,
       -event.item.quantity,
     );
+    console.log('Product updated in listener:', updatedProduct);
   }
 
+  /**
+   * On cart delete
+   * @param event
+   */
   @OnEvent('cart.deleted', { async: true })
-  handleCartDeleted(event: { items: { pid: number; quantity: number }[] }) {
+  async handleCartDeleted(event: {
+    items: { pid: number; quantity: number }[];
+  }) {
     // TODO: Improve to execute at one query
     for (const item of event.items) {
       await this.productsService.updateQuantity(item.pid, item.quantity);
     }
   }
 
+  /**
+   * On cart create
+   * @param event
+   */
   @OnEvent('cart.created', { async: true })
   handlerCartCreate(event: any) {
     console.log('cart created', event);
   }
+
+  /**
+   * On cart closed
+   * @param event
+   */
   @OnEvent('cart.closed', { async: true })
   handlerCartClosed(event: any) {
     console.log('cart closed', event);
